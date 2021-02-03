2befc7f3eb0f314770405609b0ef470153d5718d
BME Group <78388210+blockchainmusicentertainment@users.noreply.github.com>
2021-02-03 13:34:43 -0500

    Update BondageInterface.sol

--- platform/bondage/BondageInterface.sol
+++ platform/bondage/BondageInterface.sol
@@ -7,10 +7,10 @@ contract BondageInterface {
     function escrowDots(address, address, bytes32, uint256) external returns (bool);
     function releaseDots(address, address, bytes32, uint256) external returns (bool);
     function returnDots(address, address, bytes32, uint256) external returns (bool success);
-    function calcZapForDots(address, bytes32, uint256) external view returns (uint256);
+    function calcMP3ForDots(address, bytes32, uint256) external view returns (uint256);
     function currentCostOfDot(address, bytes32, uint256) public view returns (uint256);
     function getDotsIssued(address, bytes32) public view returns (uint256);
     function getBoundDots(address, address, bytes32) public view returns (uint256);
-    function getZapBound(address, bytes32) public view returns (uint256);
+    function getMP3Bound(address, bytes32) public view returns (uint256);
     function dotLimit( address, bytes32) public view returns (uint256);
 }
